2568dcf262d03a0df6dc337a117af7da1fda1de2
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-08-16 20:54:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from konlpy.tag import Okt
+from Konlpy.tag import Okt
 
 
 MAX_LEN = 30
